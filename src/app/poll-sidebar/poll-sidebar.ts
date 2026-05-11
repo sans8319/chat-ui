434177216264127b3f71f745cbbebe04faa981a9
Sanskriti Gupta <cs.sanskriti@gmail.com>
2026-05-11 10:52:07 +0530

    safty push ko feature update

---
+++ src/app/poll-sidebar/poll-sidebar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-sidebar',
+  imports: [],
+  templateUrl: './poll-sidebar.html',
+  styleUrl: './poll-sidebar.scss',
+})
+export class PollSidebar {}
