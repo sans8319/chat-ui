d1f28f6ed90955245c0126b66ed1f0d3f5601bc0
Sanskriti Gupta <cs.sanskriti@gmail.com>
2026-05-15 13:25:09 +0530

    single config1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,11 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // Change detection ko optimize kiya gaya hai
+  
     provideZoneChangeDetection({ eventCoalescing: true }),
-    // ViewTransitions se page switch hote waqt white flash nahi aayega
+
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-    // withFetch() modern browser support aur speed ke liye hai
+   
     provideHttpClient(withFetch()), 
     provideClientHydration(withEventReplay())
   ]
